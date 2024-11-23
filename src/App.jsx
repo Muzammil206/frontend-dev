1dea7ceca15259fa0de77097411404c40a5b349c
Ahmad Mustapha <ahmadishola12@gmail.com>
2024-11-23 09:28:23 -0800

    login and register page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 // import Login from "./Pages/login/Login";
 import Login from "./Pages/login/Login";
+import Register from "./Pages/Register";
 import "font-awesome/css/font-awesome.min.css";
 import AboutPage from "./Pages/AboutPage";
 
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
           <Route path="/about" element={<AboutPage />} />
         </Routes>
       </Router>
