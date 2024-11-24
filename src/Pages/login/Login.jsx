6c85a678151e3ab71c236f5195550c77b65cab1a
Usman Abdullahi <ua67527056@gmail.com>
2024-11-24 11:40:28 +0100

    login fixed: usestate importation

--- src/Pages/login/Login.jsx
+++ src/Pages/login/Login.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "../../Component/Nav";
 import Footer from "../../Component/Footer";
 import Loginimg from '../../assets/login.png'
