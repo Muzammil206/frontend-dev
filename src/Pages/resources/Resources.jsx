0fd6ad48841086f704c6cd8dab8e0e9c4986fbb8
Ahmad-mustapha <ahmadishola12@gmail.com>
2025-03-09 15:56:27 +0100

    Created resources page

--- src/Pages/resources/Resources.jsx
+++ src/Pages/resources/Resources.jsx
@@ -1,17 +1,16 @@
 import Nav from "../../Component/Nav"
 import { Hero } from "../../Component/Hero"
 import Footer from "../../Component/Footer"
-// import Eachresources from "./Eachresources"
+import Eachresources from "./Eachresources"
 const Resources = () => {
   return (
     <div>
-        {/* <Nav />
+        <Nav />
         <Hero />
         <main className="md:px-20 px-6 pb-10 mb-14">
             <Eachresources />
         </main>
-        <Footer /> */}
-        Resources
+        <Footer />
     </div>
   )
 }
